095d579f42563e5ee3141f6ba836d8e1e2dd4d57
Edo Freriks <edofre@gmail.com>
2016-12-13 16:20:42 +0100

    Removed autocomplete laravel5, makes everything slow

--- zshrc/plugins/laravel5/laravel5.plugin.zsh
+++ zshrc/plugins/laravel5/laravel5.plugin.zsh
@@ -1,18 +1,3 @@
-# Laravel5 basic command completion
-_laravel5_get_command_list () {
-	php artisan --no-ansi | sed "1,/Available commands/d" | awk '/^ +[a-z]+/ { print $1 }'
-}
-
-_laravel5 () {
-  if [ -f artisan ]; then
-    compadd `_laravel5_get_command_list`
-  fi
-}
-
-compdef _laravel5 artisan
-compdef _laravel5 la5
-
-#Alias
 alias la5='mamphp artisan'
 
 alias la5cache='mamphp artisan cache:clear'
