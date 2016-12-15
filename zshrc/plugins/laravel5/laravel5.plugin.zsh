5a9c65e9ea160812e48212b76362d25e73932a13
Edo Freriks <edofre@gmail.com>
2016-12-15 11:30:00 +0100

    Shortened la5 aliasses

--- zshrc/plugins/laravel5/laravel5.plugin.zsh
+++ zshrc/plugins/laravel5/laravel5.plugin.zsh
@@ -1,5 +1,6 @@
 alias la5='mamphp artisan'
 
-alias la5cache='mamphp artisan cache:clear'
-alias la5routes='mamphp artisan route:list'
-alias la5vendor='mamphp artisan vendor:publish'
+alias la5c='mamphp artisan cache:clear'
+alias la5r='mamphp artisan route:list'
+alias la5v='mamphp artisan vendor:publish'
+alias la5m='mamphp artisan migrate'
